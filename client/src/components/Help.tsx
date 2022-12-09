b35f179ff9adb184a30bec3021b1dfffba4af23c
Lbitwd <yang.shuya1@northeastern.edu>
2022-12-09 09:40:31 -0500

    Events

--- client/src/components/Help.tsx
+++ client/src/components/Help.tsx
@@ -6,39 +6,39 @@ import RequestQuoteIcon from '@mui/icons-material/RequestQuote';
 
 const Help = () => {
   return (
-    <div className='flex flex-col relative mx-4 lg:mx-0 xl:mx-4 my-20 max-w-screen-2xl'>
-      <h1 className='text-2xl sm:text-3xl lg:text-4xl text-primary font-heading text-center font-bold tracking-widest mb-20'>
+    <div className='relative flex flex-col mx-4 my-20 lg:mx-0 xl:mx-4 max-w-screen-2xl'>
+      <h1 className='mb-20 text-2xl font-bold tracking-widest text-center sm:text-3xl lg:text-4xl text-primary font-heading'>
         HOW CAN WE HELP YOU?
       </h1>
-      <div className='my-5 flex flex-wrap justify-center gap-12'>
+      <div className='flex flex-wrap justify-center gap-12 my-5'>
         <div className='flex flex-col items-center gap-6 max-w-[200px]'>
           <SearchIcon sx={{ fontSize: 80 }} className='text-primary' />
-          <h2 className='text-lg sm:text-xl lg:text-2xl font-bold text-primary '>SEARCH</h2>
-          <p className='text-justify text-stone-600 font-medium text-sm sm:text-base lg:text-lg'>
+          <h2 className='text-lg font-bold sm:text-xl lg:text-2xl text-primary '>SEARCH</h2>
+          <p className='text-sm font-medium text-justify text-stone-600 sm:text-base lg:text-lg'>
             Our vision is to rescue, rehabilitate and re-home animals in crisis,
             ensure healthy & good pets through education and advocacy.
           </p>
         </div>
         <div className='flex flex-col items-center gap-6 max-w-[200px]'>
           <AppRegistrationIcon sx={{ fontSize: 80 }} className='text-primary' />
-          <h2 className='text-lg sm:text-xl lg:text-2xl font-bold text-primary '>REGISTER</h2>
-          <p className='text-justify text-stone-600 font-medium text-sm sm:text-base lg:text-lg'>
+          <h2 className='text-lg font-bold sm:text-xl lg:text-2xl text-primary '>REGISTER</h2>
+          <p className='text-sm font-medium text-justify text-stone-600 sm:text-base lg:text-lg'>
             Our vision is to rescue, rehabilitate and re-home animals in crisis,
             ensure healthy pets through education, advocacy and affordable.
           </p>
         </div>
         <div className='flex flex-col items-center gap-6 max-w-[200px]'>
           <VolunteerActivismIcon sx={{ fontSize: 80 }} className='text-primary' />
-          <h2 className='text-lg sm:text-xl lg:text-2xl font-bold text-primary '>ADOPT</h2>
-          <p className='text-justify text-stone-600 font-medium text-sm sm:text-base lg:text-lg'>
+          <h2 className='text-lg font-bold sm:text-xl lg:text-2xl text-primary '>ADOPT</h2>
+          <p className='text-sm font-medium text-justify text-stone-600 sm:text-base lg:text-lg'>
             Our vision is to rescue, rehabilitate and rehome animals in crisis,
             ensure healthy pets through education, advocacy and affordable.
           </p>
         </div>
         <div className='flex flex-col items-center gap-6 max-w-[200px]'>
           <RequestQuoteIcon sx={{ fontSize: 80 }} className='text-primary' />
-          <h2 className='text-lg sm:text-xl lg:text-2xl font-bold text-primary '>DONATE</h2>
-          <p className='text-justify text-stone-600 font-medium text-sm sm:text-base lg:text-lg'>
+          <h2 className='text-lg font-bold sm:text-xl lg:text-2xl text-primary '>DONATE</h2>
+          <p className='text-sm font-medium text-justify text-stone-600 sm:text-base lg:text-lg'>
             Our vision is to rescue, rehabilitate and rehome animals in crisis,
             ensure healthy pets through education, advocacy and affordable.
           </p>
--- client/src/pages/Events.tsx
+++ client/src/pages/Events.tsx
@@ -1,18 +1,29 @@
 import React from 'react';
 import Logo from '../components/Logo';
-import bear from '../assets/images/bear.jpg';
+// import bear from '../assets/images/bear.jpg';
 import deer from '../assets/images/deer.jpg';
 import blob from '../assets/images/blob.svg';
 import Help from '../components/Help';
 import { Link } from 'react-router-dom';
 import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace';
 import { motion } from 'framer-motion';
+import LatestNews from '../components/LatestNews';
+import Events from '../components/Events';
+import OurProject from '../components/OurProject';
+const headingVariant = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      duration: 2,
+      delay: 0.5,
+    },
+  },
+};
 
-const pText1 =
-  'We at Bezubaan are working as the voice of the voiceless. We strongly believe that animals deserve to live their life free of pain and full of hope and love. Therefore, we try to give them the life we believe they deserve on a daily basis. Unfortunately, in Pakistan animals are stray with no one to take care of them or are being abandoned by their owners. Many of them become ill, injure or suffer abuses on the streets and require doctors, medicine, surgeries, and shelter to survive. Therefore, Bezubaan is the right place to report any abuse you see around your home, office or any other location.';
-const pText2 =
-  'Our vision is to rescue, rehabilitate and rehome animals in crisis, ensure healthy pets through education, advocacy and affordable services, and inspire a community where the animal-human bond is celebrated and nurtured. One day, we hope to see no animal left abandoned or being tortured in the streets of Pakistan and each one of them to have a place they could call home.';
-const About = () => {
+const EventsPage = () => {
   return (
     <motion.div
       initial={{ opacity: 0 }}
@@ -24,14 +35,14 @@ const About = () => {
       <div className=' bg-aboutImg bg-cover bg-center min-h-[60vh] w-full'>
         <div className='w-full min-h-[60vh] flex items-center bg-primary/20'>
           <h1 className=' text-4xl sm:text-5xl lg:text-6xl font-heading font-bold max-w-[550px] tracking-wide leading-[50px] sm:leading-[60px]  lg:leading-[70px] ml-10 md:ml-20 '>
-           Events Pages
+           Recent Events
             <b className='flex items-center gap-2 pb-2 border-b-2 text-primary w-fit border-primary'>
-              Bay-Zuban <Logo />{' '}
+             Animal house <Logo />{' '}
             </b>
           </h1>
         </div>
       </div>
-      <div className='relative flex flex-col mx-4 mt-20 mb-10 lg:mx-0 xl:mx-4 max-w-screen-2xl'>
+      <div className='relative flex flex-col w-full mx-4 mt-20 mb-10 lg:mx-0 xl:mx-4 max-w-screen-2xl'>
         <Link className='absolute left-0' to='/'>
           <span className='flex items-center gap-1 text-blue-500 hover:underline '>
             {' '}
@@ -43,24 +54,11 @@ const About = () => {
           src={blob}
           alt='blob'
         />
-        <div className='relative flex flex-col py-5 mt-20'>
-          <span className='w-[40px] h-[4px] bg-primary absolute top-0 left-1'></span>
-          <h1 className='text-2xl font-medium tracking-widest sm:text-3xl lg:text-4xl font-heading '>
-            OUR MISSION
-          </h1>
-          <div className='flex flex-wrap gap-4 my-10 lg:gap-2 xl:gap-6'>
-            <img
-              className='w-full h-[400px] lg:h-auto lg:w-[30vw] object-cover object-center shadow-xl rounded-xl'
-              src={bear}
-              alt='bear'
-            />
-            <p className=' text-base sm:text-lg lg:text-xl h-full bg-stone-200 p-10 sm:p-14 lg:p-20 w-full lg:max-w-[700px]'>
-              {pText1}
-            </p>
-          </div>
-        </div>
       </div>
-      <div className='relative flex flex-col mx-4 mt-20 mb-10 lg:mx-0 xl:mx-4 max-w-screen-2xl'>
+      <Events />
+
+
+      <div className='relative flex flex-col mx-4 mt-20 mb-10 lg:mx-20 xl:mx-20 max-w-screen-2xl'>
         <img
           className='absolute opacity-20 z-[-1] w-[80vw] lg:w-[40vw] top-[-25%] left-[-25%]'
           src={blob}
@@ -69,23 +67,17 @@ const About = () => {
         <div className='relative flex flex-col py-5'>
           <span className='w-[40px] h-[4px] bg-primary absolute top-0 left-1'></span>
           <h1 className='text-2xl font-medium tracking-widest sm:text-3xl lg:text-4xl font-heading '>
-            OUR VISION
+            Lastest News
           </h1>
-          <div className='flex flex-wrap gap-4 my-10 lg:flex-row-reverse lg:gap-2 xl:gap-6'>
-            <img
-              className='w-full h-[400px] lg:h-auto lg:w-[30vw] object-cover object-center shadow-xl rounded-xl'
-              src={deer}
-              alt='deer'
-            />
-            <p className=' text-base sm:text-lg lg:text-xl h-auto bg-stone-200 p-10 sm:p-14 lg:p-20 w-full lg:max-w-[700px]'>
-              {pText2}
-            </p>
+          <div className='max-w-screen-2xl'>
+            <LatestNews />
           </div>
         </div>
       </div>
+      <OurProject/>
       <Help />
     </motion.div>
   );
 };
 
-export default About;
+export default EventsPage;
